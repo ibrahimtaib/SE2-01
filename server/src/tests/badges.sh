21d48abebcb48b410d1403819425d6178f3a2916
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-08 22:45:55 +0100

    debugging

--- server/src/tests/badges.sh
+++ server/src/tests/badges.sh
@@ -1,6 +1,7 @@
 npx jest 2> jest.txt
 passed=$(grep "Test Suites" < jest.txt | grep -o '[0-9]* passed' | sed 's/ passed//' | awk '{print $1}')
 total=$(grep "Test Suites" < jest.txt | grep -o '[0-9]* total' | sed 's/ total//' | awk '{print $1}')
-
+echo $passed
+echo $total
 anybadge -l Passed -v $passed -c lime -f passed-badge.svg -o
 anybadge -l Total -v $total -c blue -f total-badge.svg -o
