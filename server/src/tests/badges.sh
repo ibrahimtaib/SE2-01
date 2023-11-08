b1db368d4b56e44853d65af542e5cee1ce9cc351
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-08 19:54:55 +0100

    script to generate badges for tests

---
+++ server/src/tests/badges.sh
@@ -0,0 +1,6 @@
+npx jest 2> jest.txt
+passed=$(grep "Test Suites" < jest.txt | grep -o '[0-9]* passed' | sed 's/ passed//' | awk '{print $1}')
+total=$(grep "Test Suites" < jest.txt | grep -o '[0-9]* total' | sed 's/ total//' | awk '{print $1}')
+
+anybadge -l Passed -v $passed -c lime -f passed-badge.svg -o
+anybadge -l Total -v $total -c blue -f total-badge.svg -o
