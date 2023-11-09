f00e12ec1c32aa15342c31c5a5a10b4c5b32d6c8
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-11-09 18:46:07 +0100

    Added proposal insertion route

---
+++ server/src/controllers/proposals.js
@@ -0,0 +1,57 @@
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient({log:["query"]});
+
+module.exports = {
+  /**
+   * Function that returns all courses in the database.
+   * @param {}
+   * @returns {Promise<[{id: Number, COD_COURSE: Number, TITLE_COURSE: String}]>}
+   */
+  getProposals: async () => {
+    return new Promise((resolve, reject) =>
+      prisma.Proposal
+        .findMany()
+        .then((propoals) => {
+          return resolve(propoals);
+        })
+        .catch((error) => {
+          console.error(error);
+          return reject({
+            error: "An error occurred while querying the database for proposals",
+          });
+        })
+    );
+  },
+
+  createProposal: async (body) => {
+    const {title, supervisor, keywords, type, groups, description, notes, expiration, level, cds, teacher, requiredKnowledge, degree} = body;
+    return new Promise((resolve, reject) =>
+        prisma.Proposal.create({
+        data:{
+            title, 
+            supervisor,
+            keywords, 
+            type, 
+            groups, 
+            description, 
+            notes, 
+            expiration,
+            level,
+            cds,
+            teacher,
+            requiredKnowledge,
+            degree
+           }
+        })
+        .then((proposal) => {
+            return resolve(proposal);
+        })
+        .catch((error) => {
+          console.error(error);
+          return reject({
+            error: "An error occurred while creating propsal",
+          });
+        })
+    );
+  },
+};
--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const proposalsRouter = require("./proposals");
 
 // Status 404 if the route is not found
+
+
+router.use('/proposals', proposalsRouter)
 router.use("*", (req, res) => [res.status(404).send("Endpoint not found")]);
 module.exports = router;
