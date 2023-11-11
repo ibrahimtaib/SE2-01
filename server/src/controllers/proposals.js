5cee98153903e9e6611c7c5ae76f01c006d81289
manarrre <manarmlaiji5@gmail.com>
2023-11-11 17:59:34 +0100

    added getProposalsByTitle api

--- server/src/controllers/proposals.js
+++ server/src/controllers/proposals.js
@@ -3,6 +3,32 @@ const prisma = new PrismaClient({log:["query"]});
 
 module.exports = {
 
+  getProposalsByTitle: async (searchString) => {
+    //const searchWords = searchString.split(' ');
+  
+    return new Promise((resolve, reject) => {
+      prisma.Proposal
+        .findMany({
+          where: {
+            title: {
+              contains: searchString,
+              mode: "insensitive",
+            },
+          },
+        })
+        .then((proposals) => {
+          return resolve(proposals);
+        })
+        .catch((error) => {
+          console.error(error);
+          return reject({
+            error: "An error occurred while querying the database",
+          });
+        });
+    });
+  },
+  
+
     getProposalsByCDS: async (cds) => {
       return new Promise((resolve, reject) =>
         prisma.Proposal
