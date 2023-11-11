520dfb664031a6794d4a9c754c0963abba5aaf5c
manarrre <manarmlaiji5@gmail.com>
2023-11-11 22:47:58 +0100

    getProposalsByCosupervisor, getProposalByExpiration, getProposalsByType routes

--- server/src/routes/proposals.js
+++ server/src/routes/proposals.js
@@ -14,6 +14,42 @@ router.get("/title/:searchString", async (req, res) => {
   }
 });
 
+router.get("/cosupervisor/:surname", async (req, res) => {
+  const surname = req.params.surname;
+
+  try {
+    const proposals = await proposalsController.getProposalsByCosupervisor(surname);
+    res.status(200).json(proposals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/type/:type", async (req, res) => {
+  const type = req.params.type;
+
+  try {
+    const proposals = await proposalsController.getProposalsByType(type);
+    res.status(200).json(proposals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/expiration/:date", async (req, res) => {
+  const date = req.params.date;
+
+  try {
+    const proposals = await proposalsController.getProposalsByExpirationDate(date);
+    res.status(200).json(proposals);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
 
 router.get("/cds/:cds", async (req, res) => {
     const cds = req.params.cds;
@@ -26,5 +62,7 @@ router.get("/cds/:cds", async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   });
+
+
 
 module.exports = router;
