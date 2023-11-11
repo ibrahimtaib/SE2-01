0abc3c917bc74bdcc9fc59a594bf5627c60bd15c
manarrre <manarmlaiji5@gmail.com>
2023-11-11 12:35:05 +0100

    adding getProposalsByCDS() route

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const proposalsRouter = require("./proposals")
 
+router.use('/proposals', proposalsRouter)
 // Status 404 if the route is not found
 router.use("*", (req, res) => [res.status(404).send("Endpoint not found")]);
 module.exports = router;
