0951817d8809bcc5b364eee76a28327c31148b96
manarrre <manarmlaiji5@gmail.com>
2023-11-11 22:46:01 +0100

    added getProposalsByCosupervisor, getProposalByExpiration, getProposalsByType

--- server/src/controllers/proposals.js
+++ server/src/controllers/proposals.js
@@ -27,7 +27,69 @@ module.exports = {
         });
     });
   },
-  
+
+  getProposalsByCosupervisor: async (surname) => {
+    return new Promise((resolve, reject) =>
+      prisma.Proposal
+        .findMany({
+          where:{
+            coSupervisors: surname
+          }
+        })
+        .then((proposals) => {
+          return resolve(proposals);
+        })
+        .catch((error) => {
+          console.error(error);
+          return reject({
+            error: "An error occurred while querying the database",
+          }); 
+        })
+    );
+  },
+
+  getProposalsByType: async (type) => {
+    return new Promise((resolve, reject) =>
+      prisma.Proposal
+        .findMany({
+          where:{
+            type: type
+          }
+        })
+        .then((proposals) => {
+          return resolve(proposals);
+        })
+        .catch((error) => {
+          console.error(error);
+          return reject({
+            error: "An error occurred while querying the database",
+          }); 
+        })
+    );
+  },
+
+
+      getProposalsByExpirationDate: async (expirationDate) => {
+      return new Promise((resolve, reject) =>
+        prisma.Proposal
+          .findMany({
+            where: {
+              expiration: {
+                lte: new Date(expirationDate),
+              },
+            },
+          })
+          .then((proposals) => {
+            return resolve(proposals);
+          })
+          .catch((error) => {
+            console.error(error);
+            return reject({
+              error: "An error occurred while querying the database",
+            });
+          })
+      );
+    },
 
     getProposalsByCDS: async (cds) => {
       return new Promise((resolve, reject) =>
