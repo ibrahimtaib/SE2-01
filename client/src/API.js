3a5775a73c142d0653c5cf3b50a13a5288630378
redon <r.karakaci@outlook.it>
2023-11-12 23:15:18 +0100

    added function getProposalsByCosupervisor

--- client/src/API.js
+++ client/src/API.js
@@ -46,8 +46,28 @@ async function getAllProposals() {
     }
   }
 
+  async function getProposalsByCosupervisor(surname) {
+    const response = await fetch(`${URL}proposals/cosupervisor/${surname}`); // Attendere che la Promise si risolva
+    const proposals = await response.json(); // Attendere che la Promise si risolva
+    if (response.ok) {
+      return proposals.map((e) => ({
+        Supervisor:e.supervisor,
+        Cds:e.cds,
+        Teacher:e.supervisor,
+        Title: e.title,
+        CoSupervisor: e.coSupervisors,
+        Expiration: dayjs(e.expiration).format('DD/MM/YYYY'),
+        Groups:e.groups,
+        Level:e.level,
+        Type:e.type,
+        Description:e.description
+      }));
+    } else {
+      throw proposals;
+    }
+  }
   
   
 
-  const API = {getAllProposals, getProposalsByTitle};
+  const API = {getAllProposals, getProposalsByTitle, getProposalsByCosupervisor};
 export default API;
