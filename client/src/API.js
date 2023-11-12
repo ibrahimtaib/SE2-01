292bce36d218ec732f53d3716aab738f858ac022
redon <r.karakaci@outlook.it>
2023-11-12 23:14:43 +0100

    added function getProposalsByTitle

--- client/src/API.js
+++ client/src/API.js
@@ -25,8 +25,29 @@ async function getAllProposals() {
     }
   }
 
+  async function getProposalsByTitle(title) {
+    const response = await fetch(`${URL}proposals/title/${title}`); // Attendere che la Promise si risolva
+    const proposals = await response.json(); // Attendere che la Promise si risolva
+    if (response.ok) {
+      return proposals.map((e) => ({
+        Supervisor:e.supervisor,
+        Cds:e.cds,
+        Teacher:e.supervisor,
+        Title: e.title,
+        CoSupervisor: e.coSupervisors,
+        Expiration: dayjs(e.expiration).format('DD/MM/YYYY'),
+        Groups:e.groups,
+        Level:e.level,
+        Type:e.type,
+        Description:e.description
+      }));
+    } else {
+      throw proposals;
+    }
+  }
+
   
   
 
-  const API = {getAllProposals};
+  const API = {getAllProposals, getProposalsByTitle};
 export default API;
