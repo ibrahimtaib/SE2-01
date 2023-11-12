1bdb1defc4b10d8fd859cbed1a7326f74551cbf1
redon <r.karakaci@outlook.it>
2023-11-12 23:13:53 +0100

    added function getAllProposals

---
+++ client/src/API.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-useless-catch */
+import axios from 'axios';
+import dayjs from 'dayjs';
+
+const URL = 'http://localhost:3001/';
+
+async function getAllProposals() {
+    const response = await fetch(`${URL}proposals`); // Attendere che la Promise si risolva
+    const proposals = await response.json(); // Attendere che la Promise si risolva
+    if (response.ok) {
+      return proposals.map((e) => ({
+        Supervisor:e.supervisor,
+        Cds:e.cds,
+        Teacher:e.supervisor,
+        Title: e.title,
+        CoSupervisor: e.coSupervisors,
+        Expiration: dayjs(e.expiration).format('DD/MM/YYYY'),
+        Groups:e.groups,
+        Level:e.level,
+        Type:e.type,
+        Description:e.description
+      }));
+    } else {
+      throw proposals;
+    }
+  }
+
+  
+  
+
+  const API = {getAllProposals};
+export default API;
