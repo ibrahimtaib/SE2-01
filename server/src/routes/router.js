f27dba86bab4454bf38187f2263ac8d1870d13cc
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-14 18:31:56 +0100

    adding middleware

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const authRouter = require("./auth");
-
+const middleware = require("../middleware");
 router.use("/login", authRouter);
+router.use("*", middleware.verifyToken);
+router.use("/teachers/*", middleware.verifyTeacher);
+router.use("/students/*", middleware.verifyStudent);
 // Status 404 if the route is not found
 router.use("*", (req, res) => [res.status(404).send("Endpoint not found")]);
 module.exports = router;
