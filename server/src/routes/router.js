54f9ce3e9a021d78f18d0f271c30b92cac506ffd
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-14 18:29:55 +0100

    linking to main router

--- server/src/routes/router.js
+++ server/src/routes/router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const authRouter = require("./auth");
 
+router.use("/login", authRouter);
 // Status 404 if the route is not found
 router.use("*", (req, res) => [res.status(404).send("Endpoint not found")]);
 module.exports = router;
