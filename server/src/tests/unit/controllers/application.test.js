2b0c19e8c5dfe2b5cb0b191fe860d9bd5d97ba5c
mcisthn504 <S319903@studenti.polito.it>
2023-11-15 21:01:52 +0100

    ASDsd

--- server/src/tests/unit/controllers/application.test.js
+++ server/src/tests/unit/controllers/application.test.js
@@ -1,52 +1,81 @@
 // Import necessary modules and functions
-const { getDegrees } = require("../../controllers/degrees.js");
+const { application } = require("../../../controllers/application.js");
 const { PrismaClient } = require("@prisma/client");
-const prisma = require("../../controllers/prisma.js");
+const { mocked } = require("jest-mock");
+const prisma = require("../../../controllers/prisma.js");
 
-// Mocking PrismaClient
-jest.mock("../../controllers/prisma.js", () => ({
-  degree: {
-    findMany: jest.fn(() => {}),
-  },
-}));
+// Mocking the prisma module
+jest.mock('../../../controllers/prisma.js', () => {
+  const { PrismaClient } = require('@prisma/client');
+  return {
+    __esModule: true,
+    PrismaClient: jest.fn(() => new PrismaClient()),
+  };
+});
 
-describe("getDegrees function", () => {
-  // Clear all mocks after each test
-  afterAll(() => {
-    jest.clearAllMocks();
+describe('createApplication', () => {
+  let prisma;
+
+  beforeAll(() => {
+    prisma = new PrismaClient();
   });
 
-  it("should resolve with degrees from the database", async () => {
-    // Mocked degrees for the successful case
-    const mockedDegrees = [
-      { COD_DEGREE: "123", TITLE_DEGREE: "Example Degree", Student: [] },
-      // Add more mocked degrees as needed for thorough testing
-    ];
-
-    // Mock the PrismaClient and its findMany method
-    prisma.degree.findMany.mockResolvedValueOnce(mockedDegrees);
-
-    // Call the function and expect the result
-    const result = await getDegrees();
-
-    // Assert the result and that findMany was called
-    expect(result).toEqual(mockedDegrees);
-    expect(prisma.degree.findMany).toHaveBeenCalled();
+  afterAll(async () => {
+    await prisma.$disconnect();
   });
 
-  it("should reject with an error if there is a database error", async () => {
-    // Mocked error for the error case
-    const mockedError = new Error("Database error");
+  it('should create an application successfully', async () => {
+    // Mocking the necessary data for the test
+    const mockBody = {
+      comment: 'Test comment',
+      STUDENT_ID: 1,
+      PROPOSAL_ID: 1,
+    };
 
-    // Mock the PrismaClient and its findMany method
-    prisma.degree.findMany.mockRejectedValueOnce(mockedError);
+    const mockStudent = {
+      id: 1,
+      COD_DEGREE: 'someDegreeCode',
+      // Add other necessary fields for student
+    };
 
-    try {
-      await getDegrees();
-    } catch (error) {
-      // Assert the error message and that findMany was called
-      expect(error.message).toEqual("An error occurred while querying the database for degrees");
-      expect(prisma.degree.findMany).toHaveBeenCalled();
-    }
+    const mockProposal = {
+      id: 1,
+      cds: 'someDegreeCode',
+      expiration: new Date(Date.now() + 1000000), // Set a future expiration date
+      applications: [], // Assuming no applications yet
+      archived: false,
+      // Add other necessary fields for proposal
+    };
+
+    // Mock the prisma methods
+    prisma.student.findUnique.mockResolvedValue(mockStudent);
+    prisma.proposal.findUnique.mockResolvedValue(mockProposal);
+    prisma.application.create.mockResolvedValue({/* mock application data */});
+
+    // Execute the function
+    const result = await createApplication(mockBody);
+
+    // Assertions
+    expect(prisma.student.findUnique).toHaveBeenCalledWith({
+      where: { id: mockBody.STUDENT_ID },
+    });
+    expect(prisma.proposal.findUnique).toHaveBeenCalledWith({
+      where: { id: mockBody.PROPOSAL_ID },
+      include: { applications: { where: { status: 'accepted' } } },
+    });
+    expect(prisma.application.create).toHaveBeenCalledWith({
+      data: {
+        status: 'pending',
+        comment: mockBody.comment,
+        STUDENT_ID: mockBody.STUDENT_ID,
+        PROPOSAL_ID: mockBody.PROPOSAL_ID,
+      },
+    });
+
+    // Add more assertions as needed based on your specific logic
+    expect(result).toBeDefined();
+    // Add more expectations based on the result
   });
+
+  // Add more test cases for different scenarios, edge cases, and error cases
 });
