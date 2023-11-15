fa2f7903fc5ebbab34e7c7c32d8422465c44ea33
JAOUAD OUXHAIB s306971 <s306971@studenti.polito.it>
2023-11-15 14:13:44 +0100

    app.js

--- server/src/app.js
+++ server/src/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const helmet = require("helmet");
 const router = require("./routes/router.js");
 const app = express();
-const allowedOrigins = ['http://127.0.0.1:5174', 'http://localhost:5173']; //Changed it to make it work
+const allowedOrigins = ['http://127.0.0.1:5173', 'http://localhost:5174']; //Changed it to make it work
 
 app.use(cors({origin: allowedOrigins, credentials:true}));
 
