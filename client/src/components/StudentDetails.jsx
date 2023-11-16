7021e11804c4fceaf1a3804b1fdba6a769e3b31c
Federico Buccellato <fedebucce@gmail.com>
2023-11-16 11:22:48 +0100

    fixing scrolling

--- client/src/components/StudentDetails.jsx
+++ client/src/components/StudentDetails.jsx
@@ -31,30 +31,30 @@ const ProposalDetails = () => {
               <li className="list-item" style={{ listStyleType: 'none' }}><span className="font-semibold">Degree Code:</span> {student.student.COD_DEGREE}</li>
               <li className="list-item" style={{ listStyleType: 'none' }}><span className="font-semibold">Enrollment Year:</span> {student.student.ENROLLMENT_YEAR}</li>
             </ul>
-          <div className="overflow-y-auto max-h-32">
-            <table className="table w-full">
-              <thead>
-                <tr className="table-row">
-                  <th className="table-cell border-b px-4 py-2">Course Code</th>
-                  <th className="table-cell border-b px-4 py-2">Course Title</th>
-                  <th className="table-cell border-b px-4 py-2">CFU</th>
-                  <th className="table-cell border-b px-4 py-2">Grade</th>
-                  <th className="table-cell border-b px-4 py-2">Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                {student.student.Career.map(course => (
-                  <tr className="table-row" key={`${course.COD_COURSE}_${course.date}`}>
-                    <td className="table-cell border-b px-4 py-2">{course.COD_COURSE}</td>
-                    <td className="table-cell border-b px-4 py-2">{course.TITLE_COURSE}</td>
-                    <td className="table-cell border-b px-4 py-2">{course.CFU}</td>
-                    <td className="table-cell border-b px-4 py-2">{course.grade}</td>
-                    <td className="table-cell border-b px-4 py-2">{course.date}</td>
+            <div className="overflow-y-auto" style={{ maxHeight: '270px' }}>
+              <table className="table w-full">
+                <thead style={{ position: 'sticky', top: 0, backgroundColor: '#fff', zIndex: 2 }}>
+                  <tr className="table-row">
+                    <th className="table-cell border-b px-4 py-2">Course Code</th>
+                    <th className="table-cell border-b px-4 py-2">Course Title</th>
+                    <th className="table-cell border-b px-4 py-2">CFU</th>
+                    <th className="table-cell border-b px-4 py-2">Grade</th>
+                    <th className="table-cell border-b px-4 py-2">Date</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
+                </thead>
+                <tbody style={{ overflowY: 'auto', maxHeight: '250px' }}>
+                  {student.student.Career.map(course => (
+                    <tr className="table-row" key={`${course.COD_COURSE}_${course.date}`}>
+                      <td className="table-cell px-4 py-2">{course.COD_COURSE}</td>
+                      <td className="table-cell px-4 py-2">{course.TITLE_COURSE}</td>
+                      <td className="table-cell px-4 py-2">{course.CFU}</td>
+                      <td className="table-cell px-4 py-2">{course.grade}</td>
+                      <td className="table-cell px-4 py-2">{course.date}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
         </div>
       </div>
     </div>
