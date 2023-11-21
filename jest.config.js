73dbd920329ea6b2c99565f437d946fe3b037f1e
frabacc <baccarociccio@gmail.com>
2023-11-21 09:43:06 +0100

    test

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.{js,jsx}'],
-    coverageDirectory: 'coverage',
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-} 
-    moduleDirectories: ["node_modules", "src/controllers"],
-}
