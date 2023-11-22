8acd1375e47e036df8af9ba605555266b604d0e0
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-22 18:13:27 +0100

    removing errors when unneeded

--- server/src/controllers/application.js
+++ server/src/controllers/application.js
@@ -34,32 +34,47 @@ module.exports = {
         });
 
         if (student == null || proposal == null) {
-          throw {
+          reject({
             status: 500,
             error: "An error occurred",
-          };
+          });
+        }
+
+        // Check the student does not have any prior applications
+        const prevApplication = await prisma.application.findFirst({
+          where: {
+            STUDENT_ID,
+            PROPOSAL_ID,
+          },
+        });
+
+        if (prevApplication != null) {
+          reject({
+            status: 400,
+            error: "Student has already applied to this proposal!",
+          });
         }
 
         // Check student is suitable
         if (student.COD_DEGREE !== proposal.cds) {
-          throw {
+          reject({
             status: 400,
             error: "Student cannot apply to this proposal!",
-          };
+          });
         }
 
         // Check proposal is valid
         if (proposal.expiration <= Date.now()) {
-          throw {
+          reject({
             status: 400,
             error: "Proposal has already expired!",
-          };
+          });
         }
         if (proposal.applications.length > 0 || proposal.archived) {
-          throw {
+          reject({
             status: 400,
             error: "The proposal is no longer available",
-          };
+          });
         }
 
         // Create application
@@ -76,8 +91,8 @@ module.exports = {
       } catch (error) {
         console.error(error);
         reject({
-          status: error.status || 500,
-          error: error.error || "An error occurred",
+          status: 500,
+          error: "An error occurred",
         });
       }
     });
