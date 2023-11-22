787892d13fe53b20e635932400824e8c3c8e27de
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-22 21:48:08 +0100

    adding route to get proposal

--- server/src/routes/application.js
+++ server/src/routes/application.js
@@ -3,7 +3,6 @@ const express = require("express");
 const router = express.Router({ mergeParams: true });
 
 router.post("/", async (req, res) => {
-  console.log("QUI");
   applicationController
     .createApplication(req.body)
     .then((application) => {
@@ -14,5 +13,18 @@ router.post("/", async (req, res) => {
       res.status(error?.status !== undefined ? error.status : 500).json(error);
     });
 });
-
+router.get("/:proposalId", async (req, res) => {
+  applicationController
+    .getApplicationsByProposalId({
+      PROPOSAL_ID: req.params.proposalId,
+      STUDENT_ID: req.body.STUDENT_ID,
+    })
+    .then((applications) => {
+      res.status(200).json(applications);
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(error?.status !== undefined ? error.status : 500).json(error);
+    });
+});
 module.exports = router;
