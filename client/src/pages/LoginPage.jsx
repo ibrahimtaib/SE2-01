51a9ffbc84fe1d011b7473b7c876698f2c341c57
frabacc <baccarociccio@gmail.com>
2023-11-23 16:29:49 +0100

    docker container created and working

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,9 @@
 /* eslint-disable react/prop-types */
-import { useState } from 'react';
-import { FiEye, FiEyeOff } from 'react-icons/fi';
-import { useNavigate } from 'react-router-dom';
-import styles from "./LoginPage.module.css";
-import Form from 'react-bootstrap/Form';
+import { useState } from "react";
+import { Button, Card, Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import validator from "validator";
+import Alert from 'react-bootstrap/Alert';
 const mockUser = {
   id: 1,
   name: 'Mario Rossi',
@@ -13,9 +13,14 @@ const mockUser = {
 
 const LoginPage = ({ setUser }) => {
   const [email, setEmail] = useState('s123456@studenti.polito.it');
-  const [password, setPassword] = useState('123456');
+  const [password, setPassword] = useState('1');
   const [showPassword, setShowPassword] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [waiting, setWaiting] = useState(false);
+
+  const [emailValid, setEmailValid] = useState(true);
+  const [passwordValid, setPasswordValid] = useState(true);
 
   var teacher = 0;
 
@@ -23,21 +28,9 @@ const LoginPage = ({ setUser }) => {
     setShowPassword(!showPassword);
   };
   const navTo = useNavigate();
-  const handleLogin = async () => {
-    // Replace this with your actual authentication logic
-    console.log('Email:', email);
-    console.log('Password:', password);
-    /*const user = await pi.login(email, password);
-
-    if (user == undefined) {
-      setShowAlert(true);
-      return;
-    } 
-    if (user == null) {
-      // an error occurred
-      alert('An error occurred. Please try again later.');
-    }*/
-
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    
     if (email === "s123456@polito.it") {
       mockUser.role = 'teacher';
       mockUser.name = 'Mario Professore'
@@ -51,7 +44,7 @@ const LoginPage = ({ setUser }) => {
       navTo("/");
     }
     else {
-      alert('An error occurred. Please try again later.');
+      setErrorMessage('An error occurred while trying to login.')
     }
 
   };
@@ -59,36 +52,57 @@ const LoginPage = ({ setUser }) => {
 
   return (
     <>
-      <div className={styles["login-page"]}>
-        <div className={styles["login-container"]}>
-          <h2>Login</h2>
-          <div className={styles["input-container"]}>
-            <input
-              type="email"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className={styles["input-container"]}>
-            <input
-              type={showPassword ? 'text' : 'password'}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <span className={styles["password-icon"]} onClick={handleTogglePassword}>
-              {showPassword ? <FiEye /> : <FiEyeOff />}
-            </span>
-          </div>
-          <span className={styles["alert"]} style={{ display: showAlert ? 'block' : 'none' }}>Incorrect email or password</span>
-          <button className={styles["login-btn"]} onClick={handleLogin}>
-            Login
-          </button>
-        </div>
-      </div>
-      <div>
-      </div>
+     <Container fluid style={{ display: 'flex', justifyContent: 'center', height: '100vh', marginTop : '2rem'}}>
+        <Row className>
+        <Col style={{"maxWidth": "1rem", "minWidth": "30rem"}}>
+        <Card>
+          <Container style={{"marginTop": "0.5rem", "padding": "1rem"}}>
+          <h1 style={{textAlign : "center" , marginBottom :"1rem"}}>Thesis Management</h1>
+            <Form noValidate onSubmit={handleLogin}>
+            {errorMessage ? <Alert variant='warning' style={{width : "100%"}} dismissible onClick={()=>setErrorMessage('')}>{errorMessage}</Alert> : ''}
+              <Row className="mb-3">
+                <Form.Group as={Col}>
+                  <Form.Label>E-Mail</Form.Label>
+                  <Form.Control isInvalid={!emailValid}
+                                type="email"
+                                placeholder=""
+                                value={email}
+                                autoFocus
+                                onChange={event => {setEmail(event.target.value); setEmailValid(true);}}/>
+                  <Form.Control.Feedback type="invalid">
+                    Field is required!
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              <Row className="mb-3">
+                <Form.Group as={Col}>
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control isInvalid={!passwordValid}
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={event => {setPassword(event.target.value); setPasswordValid(true);}}/>
+                  <Form.Control.Feedback type="invalid">
+                    Field is required!
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Row>
+              {
+                waiting ? 
+                <>
+                  <Spinner as="span" animation="border" size="sm" role="status" aria-hidden="true" variant="secondary"/>
+                  {" "}
+                </>
+                
+                : <div style={{ display: "flex" , justifyContent : "center"}}><Button style={{ backgroundColor : "#003576", display :"flex", justifyContent : "flex-end" , alignContent: "flex-end"} }type="submit" variant="secondary" disabled={waiting}>Login</Button></div>
+              }
+            </Form>
+          </Container>
+        </Card>
+        </Col>
+        <Col md="3"/>
+        </Row>
+        </Container>
 
     </>
 
