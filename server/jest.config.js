44d60ef9dbbd16cb034efcbbdf44d5f3b0e54c1d
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-11-25 21:43:13 +0100

    removing prisma from coverage

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   collectCoverage: true,
   testEnvironment: "node",
-  collectCoverageFrom: ["src/**/*.{js,jsx}", "!src/test*/**", "!src/*.js"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx}",
+    "!src/test*/**",
+    "!src/*.js",
+    "!src/controllers/prisma.js",
+  ],
   coverageDirectory: "coverage",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   moduleNameMapper: {
