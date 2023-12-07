6555ad55aa676edb35be46f9855903755beeb997
Ibrahim Taib <ibrahim.taib@grenoble-inp.org>
2023-12-07 14:28:45 +0100

    virtualClock router

---
+++ server/src/routes/virtualClock.js
@@ -0,0 +1,22 @@
+const virtualClockController = require("../controllers/virtualClock");
+const express = require("express");
+const router = express.Router({ mergeParams: true });
+
+router.get("/", async (req, res) => {
+  let virtualClock = virtualClockController.getVirtualClock();
+  console.log(virtualClock);
+  res.status(200).json(virtualClock);
+});
+
+router.post("/", async (req, res) => {
+  const { newDate } = req.body;
+  let virtualClock = virtualClockController.setVirtualClock(newDate);
+  res.status(200).json(virtualClock);
+});
+
+router.delete("/", async (req, res) => {
+  let virtualClock = virtualClockController.resetVirtualClock();
+  res.status(200).json("Virtual clock reset");
+});
+
+module.exports = router;
